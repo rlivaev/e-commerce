a9c6fdc3e5a0de6a126723934c2ce9d780776e51
Rustam <ruslivaev05@gmail.com>
2023-05-02 18:25:19 +0600

    finished contact block

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Slider from '../../components/Slider/Slider';
 import './Home.scss';
 import { FeaturedProducts } from '../../components/FeaturedProducts/FeaturedProducts';
 import { Categories } from '../../components/Categories/Categories';
+import { Contact } from '../../components/Contact/Contact';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedProducts type="Featured" />
       <Categories />
       <FeaturedProducts type="Trending" />
+      <Contact />
     </div>
   );
 };
